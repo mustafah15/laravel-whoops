4cb8ef35e1b509be97e06804160d16260a5958ed
Guido van Biemen <guidovanbiemen@gmail.com>
2015-02-06 20:54:12 +0100

    PSR-2 code style.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,35 +1,34 @@
 <?php namespace Gvb\Whoops;
 
-class ServiceProvider extends \Illuminate\Support\ServiceProvider {
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var boolean
+     */
+    protected $defer = false;
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app->singleton(
-			'Illuminate\Contracts\Debug\ExceptionHandler',
-			'Gvb\Whoops\ExceptionHandler'
-		);
-	}
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+        $this->app->singleton(
+            'Illuminate\Contracts\Debug\ExceptionHandler',
+            'Gvb\Whoops\ExceptionHandler'
+        );
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return ['Illuminate\Contracts\Debug\ExceptionHandler'];
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['Illuminate\Contracts\Debug\ExceptionHandler'];
+    }
 
 }
